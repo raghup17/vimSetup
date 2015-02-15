50d60a5357431a0c358e03fd18b56eafdffab1ca
antonin <antonin>
2015-02-14 17:36:50 -0800

    Replaced usage of curl with wget, one less thing to install

--- vimSetup.sh
+++ vimSetup.sh
@@ -66,9 +66,8 @@ echo "set tags+=./.tagsRuntime;" >> ~/.vimrc
 
 ## Install pathogen
 echo "Installing pathogen"
-sudo apt-get install curl
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+  wget -O ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo "execute pathogen#infect()" >> ~/.vimrc
 echo "syntax on" >> ~/.vimrc
 echo "filetype plugin indent on" >> ~/.vimrc
